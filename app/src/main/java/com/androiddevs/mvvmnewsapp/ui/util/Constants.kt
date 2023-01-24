e923050b9a2cb261716b103f7a9751fb9cf18e06
BJIT <sadmanabir@bjitacademy.com>
2023-01-24 15:34:34 +0600

    NewsApi App

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "511403da736449f398518c2f48d9d9e8"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
